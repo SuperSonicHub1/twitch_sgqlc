b167c74201c64f135c194069c04771fbe09e0e64
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2021-01-28 18:54:35 -0500

    Added tests for regex and made some fixes.

--- tests/test_twitch_sgqlc.py
+++ tests/test_twitch_sgqlc.py
@@ -1,5 +1,20 @@
-from twitch_sgqlc import __version__
+import urllib.request
+from sgqlc.endpoint.http import HTTPEndpoint
+from sgqlc.operation import Operation
+from twitch_sgqlc import __version__, schema, get_client_id, DEFAULT_CLIENT_ID
+
+import pytest
+
+
+@pytest.fixture
+def twitch_homepage():
+    with urllib.request.urlopen("https://twitch.tv/") as f:
+        yield f.read().decode()
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_regex(twitch_homepage: str):
+    assert get_client_id(twitch_homepage) == DEFAULT_CLIENT_ID
--- twitch_sgqlc/client_id.py
+++ twitch_sgqlc/client_id.py
@@ -2,8 +2,10 @@ import re
 
 DEFAULT_CLIENT_ID = "kimne78kx3ncx6brgo4mv6wki5h1ko"
 CLIENT_ID_REGEX = r"\"Client-ID\":\"(?P<client_id>.*)\",\"Content-Type\""
+
+
 def get_client_id(text: str) -> str:
-    """Extract the Client-ID from Twitch's homepage. You probably don't need this, as it seems that Twitch has a universal ID: ``
+    """Extract the Client-ID from Twitch's homepage. You probably don't need this, as it seems that Twitch has a universal ID: `kimne78kx3ncx6brgo4mv6wki5h1ko`
 
     Args:
         text (str): HTML from Twitch's homepage
@@ -11,6 +13,6 @@ def get_client_id(text: str) -> str:
     Returns:
         str: The Client-Id, will be empty if not found.
     """
-    
-    match = re.match(CLIENT_ID_REGEX, text)
+
+    match = re.search(CLIENT_ID_REGEX, text)
     return match.group("client_id") if match else ""
