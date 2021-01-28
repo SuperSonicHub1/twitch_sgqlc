5b839f716bd7ec3fde6e6d198d8b67ad193c15d5
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2021-01-28 18:21:05 -0500

    Added default Client-ID.

--- twitch_sgqlc/client_id.py
+++ twitch_sgqlc/client_id.py
@@ -1,5 +1,6 @@
 import re
 
+DEFAULT_CLIENT_ID = "kimne78kx3ncx6brgo4mv6wki5h1ko"
 CLIENT_ID_REGEX = r"\"Client-ID\":\"(?P<client_id>.*)\",\"Content-Type\""
 def get_client_id(text: str) -> str:
     """Extract the Client-ID from Twitch's homepage. You probably don't need this, as it seems that Twitch has a universal ID: ``
